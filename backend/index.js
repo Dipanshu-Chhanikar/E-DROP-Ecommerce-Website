46479e5a319c683d9ff76ef9ee876559c1bdfb06
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2024-11-19 16:15:05 +0530

    router added

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
 
+app.use(express.json());
+app.use("/api", require("./routes/CreateUser"));
 // const PORT = process.env.PORT || 5000;
 app.listen(Port, () => {
   console.log(`Server is running on http://localhost:${Port}`);
